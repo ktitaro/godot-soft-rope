e99207552cf1b8976b7eb7956c974092f0ca5423
Anatoly Ktitarov <hello@ktitarov.com>
2023-06-02 06:16:30 +0300

    Added an example of tile swing

--- cargo.gd
+++ cargo.gd
@@ -3,3 +3,9 @@ extends RigidBody2D
 func _draw():
 	var pos=Vector2.ZERO
 	draw_circle(pos,12,Color.PLUM)
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("action_c"):
+		var dir=self.linear_velocity.normalized()
+		self.apply_impulse(dir*1000)
+
--- world.gd
+++ world.gd
@@ -1,8 +1,5 @@
 extends Node2D
 
-func _init():
-	print("BRO WTF??")
-
 func _draw():
 	var r=Rect2(0,0,320,320)
 	draw_rect(r,Color.PERU,false,8)
